0e1da7cb3b60e69f51e119898da47e311e306f03
David Woodland <65493742+davidrwoodland@users.noreply.github.com>
2025-03-29 12:44:59 +1100

    ghostty

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,14 @@
 return {
-    "ellisonleao/gruvbox.nvim",
+    "sainnhe/gruvbox-material",
     priority = 1000,
     lazy = false,
-    opts = ...,
     config = function()
-        vim.cmd('colorscheme gruvbox')
+        -- Set Gruvbox Material options
+        vim.g.gruvbox_material_background = 'hard' -- Options: 'soft', 'medium', 'hard'
+        vim.g.gruvbox_material_palette = 'material' -- Options: 'original', 'mix', 'material'
+        vim.g.gruvbox_material_better_performance = 1
+
+        -- Apply the colorscheme
+        vim.cmd('colorscheme gruvbox-material')
     end
 }
